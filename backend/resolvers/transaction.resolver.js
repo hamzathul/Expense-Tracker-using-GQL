453331b7a39dc1a7efb3e1c61536f9d3abd2df69
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-01 09:59:14 +0530

    Transaction queries are added

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,5 +1,30 @@
+import Transaction from "../models/transaction.model.js";
+
 const transactionResolver = {
-  Query: {},
+  Query: {
+    transacions: async (_, __, context) => {
+      try {
+        if (!context.getUser()) throw new Error("Unauthorized");
+        const userId = await context.getUser()._id;
+
+        const transactions = await Transaction.find({ userId });
+        return transactions;
+      } catch (error) {
+        console.error("Error getting transactions:", err)
+        throw new Error("Error getting transactions")
+      }
+    },
+    transaction: async(_,{transactionId})=>{
+      try {
+        const transaction = await Transaction.findById(transactionId)
+        return transaction
+      } catch (error) {
+        console.error("Error getting transaction:",error)
+        throw new Error("Error getting transaction")
+      }
+    },
+    //TODO => ADD CategoryStatistics query
+  },
   Mutation: {},
 };
 
--- backend/typeDefs/transaction.typeDef.js
+++ backend/typeDefs/transaction.typeDef.js
@@ -13,6 +13,7 @@ const transactionTypeDef = `#graphql
     type Query{
         transactions:[Transaction!]
         transaction(transactionId:ID): Transaction
+        #TODO => ADD CategoryStatistics query
     }
 
     type Mutation{
