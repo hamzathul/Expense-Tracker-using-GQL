38a93e2be9c9e8aa853793b173552a1afcf8164f
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-02 10:23:48 +0530

    bg grid to bg dot

--- frontend/src/components/ui/GridBackground.jsx
+++ frontend/src/components/ui/GridBackground.jsx
@@ -1,6 +1,6 @@
 const GridBackground = ({ children }) => {
   return (
-    <div className="w-full bg-black  bg-grid-white/[0.2] relative text-white">
+    <div className="w-full bg-black  bg-dot-white/[0.2] relative text-white">
       {/* Radial gradient for the container to give a faded look */}
       <div className="absolute pointer-events-none inset-0 bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
      {children}
