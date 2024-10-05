ee6d883714de59e136e7d4769ad7d28cf000febe
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-05 09:57:52 +0530

    add: profile picture in every card

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -17,14 +17,14 @@ const Cards = () => {
 
   // if(error) return <p>Error:{error.message}</p>
   // if(loading) return <p>Loading...</p>
-  console.log("cards:", data);
+  // console.log("cards:", data);
   return (
     <div className="w-full px-10 min-h-[40vh]">
       <p className="text-5xl font-bold text-center my-10">History</p>
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-start mb-20">
         {!loading &&
           data.transactions.map((transaction) => (
-            <Card key={transaction._id} transaction={transaction} />
+            <Card key={transaction._id} transaction={transaction} authUser={authUser.authUser}/>
           ))}
       </div>
       {!loading && data?.transactions?.length == 0 && (
