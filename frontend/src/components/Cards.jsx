f23ed3c21cc3de9cb62df461a8ea0ebfa3a466c0
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-05 09:24:10 +0530

    add: user and transaction relation frontend

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -1,9 +1,19 @@
 import { useQuery } from "@apollo/client";
 import Card from "./Card";
 import { GET_TRANSACTIONS } from "../graphql/queries/transaction.query";
+import {
+  GET_AUTHENTICATED_USER,
+  GET_USER_AND_TRANSACTIONS,
+} from "../graphql/queries/user.query";
 
 const Cards = () => {
   const { data, loading, error } = useQuery(GET_TRANSACTIONS);
+  const { data: authUser } = useQuery(GET_AUTHENTICATED_USER);
+  const { data: userAndTransactions } = useQuery(GET_USER_AND_TRANSACTIONS, {
+    variables: { userId: authUser?.authUser?._id },
+  });
+
+  console.log("User and Transactions", userAndTransactions);// to learn the relation
 
   // if(error) return <p>Error:{error.message}</p>
   // if(loading) return <p>Loading...</p>
