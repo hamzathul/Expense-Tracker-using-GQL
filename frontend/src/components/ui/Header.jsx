2d1d9f3b0b477d8a440f8e65fc1d56638031daed
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-08 12:04:00 +0530

    update: app name

--- frontend/src/components/ui/Header.jsx
+++ frontend/src/components/ui/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="mb-10">
       <h1 className="md:text-6xl text-4xl lg:text-6xl font-bold text-center  relative z-50 text-white pt-10">
-        Expense Tracker <Link to="/">GQL</Link>
+        Expense Tracker using <Link to="/">GQL</Link>
       </h1>
       <div className="relative mb-10 w-1/2 mx-auto hidden md:block">
         {/* Gradients */}
