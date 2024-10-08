233a223ef353137ca3cb79db6b53b5d18dc13ab3
Hamzathul Favas E <ehamzathulfavas@gmail.com>
2024-10-08 10:07:21 +0530

    Add: hr in UI

--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -21,10 +21,15 @@ const Cards = () => {
   return (
     <div className="w-full px-10 min-h-[40vh]">
       <p className="text-5xl font-bold text-center my-10">History</p>
+      <hr class="my-12 h-0.5 border-t-0 bg-gray-600" />
       <div className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 justify-start mb-20">
         {!loading &&
           data.transactions.map((transaction) => (
-            <Card key={transaction._id} transaction={transaction} authUser={authUser.authUser}/>
+            <Card
+              key={transaction._id}
+              transaction={transaction}
+              authUser={authUser.authUser}
+            />
           ))}
       </div>
       {!loading && data?.transactions?.length == 0 && (
